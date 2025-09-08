113a92469660019686c8cd7f53de79760e639378
Tio Haidar <atiohaidar@gmail.com>
2025-09-08 19:35:30 +0700

    hilangin fitur scan qr nya dulu

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ function App() {
       fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif'
     }}>
 
-      {/* Tombol Scan QR di pojok kanan atas */}
+      {/* Tombol Scan QR di pojok kanan atas
       <button
         onClick={() => setShowScanner(true)}
         style={{
@@ -210,7 +210,7 @@ function App() {
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{marginRight:8,verticalAlign:'middle'}}><rect x="3" y="3" width="7" height="7" rx="1"/><rect x="14" y="3" width="7" height="7" rx="1"/><rect x="14" y="14" width="7" height="7" rx="1"/><rect x="3" y="14" width="7" height="7" rx="1"/></svg>
         Scan QR
-      </button>
+      </button> */}
 
       {/* Modal QRScanner */}
       {showScanner && (
